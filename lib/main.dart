50d5405dd48913a5c9bac16aa386a8c349db2191
Samer Neiroukh <samer.neiroukh@gmail.com>
2025-01-10 19:15:00 +0200

    better looking personal area

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-    
 import 'package:firebase_core/firebase_core.dart';
-    
-
-  
 import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/personal_area_screen.dart';
-    
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
