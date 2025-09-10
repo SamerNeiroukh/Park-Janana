1d6f584bb076b2e81bc078d28c0b39bdbcba1768
SamerNeiroukh <119084999+SamerNeiroukh@users.noreply.github.com>
2025-09-10 17:45:19 +0300

    Upgrading project's tools to their latest versions. (#32)
    * Update Gradle configuration for improved performance and compatibility
    
    * Update dependencies and generated plugin registrations for improved functionality
    
    * Set minSdkVersion to 23 in build.gradle
    
    * Remove firebase_app_check dependency and its related imports
    
    ---------
    
    Co-authored-by: Mousa Tams <mousatams@hotmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,11 +7,11 @@ import Foundation
 
 import cloud_firestore
 import file_selector_macos
-import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_storage
 import geolocator_apple
+import package_info_plus
 import path_provider_foundation
 import printing
 import shared_preferences_foundation
@@ -20,11 +20,11 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
